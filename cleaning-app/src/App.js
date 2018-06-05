019e46aaaf3a6c38f8b496f9480e513f1ff0d99d
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-06-05 18:18:15 -0500

    setting up bootstrap

--- cleaning-app/src/App.js
+++ cleaning-app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">The Rock Booking</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a test, App Coming Soon!!!
         </p>
       </div>
     );
