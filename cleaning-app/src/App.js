c2110a3193866d97318e4fc90279801b4a0778c1
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-06-05 19:46:56 -0500

    testing bootstrap

--- cleaning-app/src/App.js
+++ cleaning-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
           <h1 className="App-title">The Rock Booking</h1>
         </header>
         <p className="App-intro">
