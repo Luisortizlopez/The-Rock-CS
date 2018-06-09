13ca35261faa80ded740175b3d9d83cda9f29023
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-06-09 15:44:35 -0500

    react-router is working

--- cleaning-app/src/App.js
+++ cleaning-app/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap';
+
+import Home from './pages/Home.jsx';
 
 class App extends Component {
   render() {
     return (
+      <Router>
+        <div>
+      <Route exact path="/" component={Home}/>
+        </div>
+      </Router>
 
     );
   }
