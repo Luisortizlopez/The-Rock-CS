7185134527576928d8cb74f8da05eb2139a10a41
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-06-09 14:19:04 -0500

    adding react-router

--- cleaning-app/src/App.js
+++ cleaning-app/src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import 'bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
 
-          <h1 className="App-title">The Rock Booking</h1>
-        </header>
-        <p className="App-intro">
-          This is a test, App Coming Soon!!!
-        </p>
-      </div>
     );
   }
 }
